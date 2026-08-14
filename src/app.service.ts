df47001fd8e59027c67d6537007e80c9cc71499c
Gunasheelan Arullchelvan <140253087+Gunasheelan24@users.noreply.github.com>
2026-08-14 22:09:16 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly uploadPath = path.join(process.cwd(), 'storage');
+  
   getHello(): string {
     return 'Hello World!';
-  }
+  };
+
+  
+  createFolder(folderName: string) {
+    const folderPath = path.join(this.uploadPath, folderName);
+
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true });
+    }
+
+    return {
+      message: 'Folder created successfully',
+      path: folderPath,
+    };
+  };
 }
